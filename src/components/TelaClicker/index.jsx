bd75b48cbd7e3fd1d1999ba292d1065098b3a1c8
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-11 13:47:20 -0300

    Estilizando botão de clique

--- src/components/TelaClicker/index.jsx
+++ src/components/TelaClicker/index.jsx
@@ -1,9 +1,10 @@
 import { IoIosFootball } from "react-icons/io";
+import './tela-clicker.css';
 
 export default function TelaClicker({ noClique }) {
     return (
         <div className="clicker-container">
-            <button onClick={noClique}><IoIosFootball /></button>
+            <button className="botao-click" onClick={noClique}><IoIosFootball /></button>
         </div>
     )
 }
