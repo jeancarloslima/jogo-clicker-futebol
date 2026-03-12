c5df9f1029faf4296ec476e1f7a079512f5e49eb
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-12 11:18:37 -0300

    Adicionando ícones ao cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import TelaClicker from './components/TelaClicker';
 import TelaTime from './components/TelaTime';
 import TelaMelhorias from './components/TelaMelhorias';
 import TelaJogadores from './components/TelaJogadores';
+import { FaCalendar } from "react-icons/fa6";
+import { RiMoneyDollarCircleFill } from 'react-icons/ri';
+import { GiSoccerKick } from 'react-icons/gi';
 
 function App() {
   const [count, setCount] = useState(0)
   const [diasFaltando, setDiasFaltando] = useState(30);
+  const [forcaTime, setForcaTime] = useState(100);
   const [telaAtual, setTelaAtual] = useState(0);
 
   function handleClick() {
@@ -18,8 +22,9 @@ function App() {
     <div className="app">
       <header>
         <div className="header-container">
-          <h2 className="contagem">{count}</h2>
-          <h3 className="dias-faltando">{diasFaltando} dias</h3>
+          <h2 className="contagem">{count} <RiMoneyDollarCircleFill /></h2>
+          <h3 className="dias-faltando">{diasFaltando} <FaCalendar /></h3>
+          <h3 className="forca-time">{forcaTime} <GiSoccerKick /></h3>
         </div>
       </header>
 
--- src/components/TelaMelhorias/index.jsx
+++ src/components/TelaMelhorias/index.jsx
@@ -1,3 +1,7 @@
 export default function TelaMelhorias() {
-    
+    return (
+        <div className="melhorias-container">
+            
+        </div>
+    )
 }
