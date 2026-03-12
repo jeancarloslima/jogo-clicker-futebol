c78ee93cf1ffc29c6b1c1a85f0b42f651f512b1f
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-12 11:58:44 -0300

    Ajustando design da tela de melhorias

--- src/components/TelaMelhorias/index.jsx
+++ src/components/TelaMelhorias/index.jsx
@@ -1,6 +1,7 @@
 import './tela-melhorias.css';
 import { RiMoneyDollarCircleFill } from 'react-icons/ri';
 import { IoIosFootball } from 'react-icons/io';
+import { GiSoccerKick } from 'react-icons/gi';
 
 export default function TelaMelhorias() {
     return (
@@ -14,14 +15,17 @@ export default function TelaMelhorias() {
                 <li className="item-melhoria">
                     <span className='nome-melhoria'>Pedir dinheiro a vizinhos</span>
                     <span className="custo-melhoria">50 <RiMoneyDollarCircleFill /></span>
+                    <span className="efeito-melhoria">+0.5 <IoIosFootball /></span>
                 </li>
                 <li className="item-melhoria">
-                    <span className='nome-melhoria'></span>
+                    <span className='nome-melhoria'>Treino de passe</span>
                     <span className="custo-melhoria">100 <RiMoneyDollarCircleFill /></span>
+                    <span className="efeito-melhoria">+50 <GiSoccerKick /></span>
                 </li>
                 <li className="item-melhoria">
                     <span className='nome-melhoria'>Investidor local</span>
                     <span className="custo-melhoria">300 <RiMoneyDollarCircleFill /></span>
+                    <span className="efeito-melhoria">+2 <IoIosFootball /></span>
                 </li>
             </ul>
         </div>
